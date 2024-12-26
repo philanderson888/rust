2d4bb681288dbc5c6f4e1dbdfd11128e4d571ab5
philanderson888 <philanderson888@hotmail.com>
2024-12-26 05:38:03 +0000

    Add initial implementation of Grep sample application with command line argument handling and file reading

---
+++ projects/grep/grep-01/src/main.rs
@@ -0,0 +1,60 @@
+use std::env;
+use std::fs;
+fn main() {
+    println!("==============================================================");
+    println!("==============================================================");
+    println!("====               Grep Sample Application                ====");
+    println!("==============================================================");
+    println!("==============================================================");
+
+    println!("==============================================================");
+    println!("====                   Introduction                       ====");
+    println!("==============================================================");
+
+    println!("Grep is a command-line utility for searching plain-text data sets for lines that match a regular expression");
+    println!("Its name comes from the ed command g/re/p (globally search a regular expression and print)");
+    println!("which has the same effect: doing a global search with the regular expression and printing all matching lines");
+
+    println!("\n... the idea will be to combine the learning to date to build a real-world application");
+
+    println!("\ninputs: path to file, string to search for");
+    println!("\noutputs: lines containing the string");
+    println!("\nconfiguration: environment variables, command-line arguments ... ");
+    println!("\n... eg case sensitive/insensitive search, whole word search, line numbers, file names, etc");
+    println!("\nerror handling: invalid inputs, invalid UTF-8, file not found");
+
+    println!("\n==============================================================");
+    println!("====              Accepting Command Line Arguments        ====");
+    println!("==============================================================");
+
+    println!("\n... the first step is to accept command line arguments");
+
+    let args: Vec<String> = env::args().collect();
+    dbg!(&args);
+    println!("args: {:?}", args);
+
+    //let query = &args[1];
+    //let filename = &args[2];
+
+    //println!("Searching for '{}'", query);
+    //println!("In file '{}'", filename);
+
+    println!("\n==============================================================");
+    println!("====              Reading the File                        ====");
+    println!("==============================================================");
+
+    println!("\n... the next step is to read the file");
+
+    let filename = "src/poem.txt";
+
+    println!("\nfile path '{}'", filename);
+
+    let contents = fs::read_to_string(filename)
+        .expect("Something went wrong reading the file");
+
+    println!("\nfile contents:\n{}", contents);
+
+
+    
+    
+}
