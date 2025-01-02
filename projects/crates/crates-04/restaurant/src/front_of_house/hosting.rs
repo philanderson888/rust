7b6596d1abd01c9f04cdac54652059693564354f
philanderson888 <philanderson888@hotmail.com>
2025-01-02 14:24:41 +0000

    renaming projects

--- projects/crates/crates-04/restaurant/src/front_of_house/hosting.rs
+++
@@ -1,8 +0,0 @@
-// with pub use this is directly accessible in the crate ie restaurant::hosting::add_to_waitlist
-pub fn add_to_waitlist() {
-    println!("add_to_waitlist");
-}
-
-pub fn seat_at_table() {
-    println!("seat_at_table");
-}
