1e0e131f847d2072df93e16af5c4803ac478c5f7
philanderson888 <philanderson888@hotmail.com>
2025-01-12 20:42:35 +0000

    add going deeper project

---
+++ projects/23-going-deeper/src/main.rs
@@ -0,0 +1,21 @@
+fn main() {
+
+
+    println!("==============================================================");
+    println!("==============================================================");
+    println!("====                  Going Deeper                        ====");
+    println!("==============================================================");
+    println!("==============================================================");
+
+    println!("==============================================================");
+    println!("====              Associated Items                        ====");
+    println!("==============================================================");
+
+    println!("... associated items are defined in a trait or an impl block");
+    println!("... each associated item either declares a signature declaration or the actual implementation code ... ")
+    println!("\n... we can have ... ");
+    println!("... - associated types eg type Item;");
+    println!("... - associated constants eg const MAX_ITEMS: u32;");
+    println!("... - associated functions eg fn new() -> Self;");
+    
+}
